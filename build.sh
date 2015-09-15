85117a6ef291e14ad040564135ae29282930706c
runseb <runseb@gmail.com>
2015-09-15 18:31:26 +0200

    trying to clean license headers

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#   Copyright {yyyy} {name of copyright owner}
+#   Copyright 2015 Sebastien Goasguen
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
